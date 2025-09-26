da8987c16fd74b6d83a5c606cb7f3f18a9548d84
Terry <t3rryahn.dev@gmail.com>
2025-09-26 22:52:09 +0900

    fix: setId() 주석 처리, getId() 주석 해제

--- ssafit/src/main/java/com/ssafy/ssafit/model/Video.java
+++ ssafit/src/main/java/com/ssafy/ssafit/model/Video.java
@@ -31,13 +31,13 @@ public class Video {
 	//////////////////////////////////////////////////
 	// getter setters
 	
-//	public String getId() {
-//		return id;
-//	}
-
-	public void setId(String id) {
-		this.id = id;
+	public String getId() {
+		return id;
 	}
+
+//	public void setId(String id) {
+//		this.id = id;
+//	}
 
 	public String getTitle() {
 		return title;
